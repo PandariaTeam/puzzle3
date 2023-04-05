d0c7eb56185524378f510d710e9c1cddfd7423ec
ltProcess <lt41156004@163.com>
2023-04-05 23:48:23 +0800

    feat: puzzle form ui

--- web/src/pages/user/index.tsx
+++ web/src/pages/user/index.tsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
 import { Button } from 'antd';
 import { useStore } from '@/context';
+import PuzzleForm from '@/components/Form';
+import { userCls } from './style';
 
 function User() {
   const {
@@ -15,9 +17,10 @@ function User() {
   }, []);
   const onSubmit = () => userStore.create();
   return (
-    <div>
+    <div className={userCls}>
+      <PuzzleForm />
       <p onClick={() => userStore.changeName()}>{userStore.name}</p>
-      <Button onClick={onSubmit}>提交</Button>
+      <Button onClick={onSubmit}>测试</Button>
     </div>
   );
 }
