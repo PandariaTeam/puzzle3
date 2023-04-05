73f03ba3a972595a11579b7fae3c9ddbb050b2c1
Jeason <me@cowpoke.cc>
2023-04-05 15:12:06 +0800

    chore: add contracts

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,18 @@
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import { ACCOUNT_PRIVATE_KEY, INFURA_API_KEY, ETHERSCAN_API_KEY } from './env';
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.19',
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [ACCOUNT_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
