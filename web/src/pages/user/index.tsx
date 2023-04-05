9a763cbcf5bd8513ec150a57ee458f463277196d
ltProcess <lt41156004@163.com>
2023-04-05 17:41:37 +0800

    feat: update api

--- web/src/pages/user/index.tsx
+++ web/src/pages/user/index.tsx
@@ -1,10 +1,15 @@
+import { useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import { useStore } from '@/context';
+import { getMetaDataById } from '@/services/api';
 
 function User() {
   const {
     rootStore: { userStore }
   } = useStore();
+  useEffect(() => {
+    getMetaDataById('2597505e-4e89-4737-98a4-fa3368359a4d');
+  }, []);
   return (
     <div>
       <p onClick={() => userStore.changeName()}>{userStore.name}</p>
