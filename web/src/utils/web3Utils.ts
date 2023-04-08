254bc2498f79da2ab183c79e4dfdbb029b188630
ltProcess <lt41156004@163.com>
2023-04-08 12:51:07 +0800

    fix: contractAddress

--- web/src/utils/web3Utils.ts
+++ web/src/utils/web3Utils.ts
@@ -1,2 +1,2 @@
 export const chainId = '0xaa36a7';
-export const contractAddress = '0xb0a14E64E6a74A397fbaA94cC700158Bf655BD99';
+export const contractAddress = '0x1a86339ad7fBB50F753A0Eb44B0FdfD4BC931A44';
