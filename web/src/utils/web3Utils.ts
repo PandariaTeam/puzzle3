865788b27f106338f81ac3e03d66e2d3d4eca282
ltProcess <lt41156004@163.com>
2023-04-08 13:54:28 +0800

    fix: u

--- web/src/utils/web3Utils.ts
+++ web/src/utils/web3Utils.ts
@@ -1,5 +1,5 @@
 export const isScroll = window.location.host === 'scroll-alpha.puzzle3.cc';
-export const chainId = isScroll ? '82751' : '0xaa36a7';
+export const chainId = isScroll ? '0x82751' : '0xaa36a7';
 export const contractAddress = isScroll
   ? '0xA511432602607a566d3C5603Dcb30FfEE0C2b996'
   : '0x1a86339ad7fBB50F753A0Eb44B0FdfD4BC931A44';
